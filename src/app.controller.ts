06b827180a67f3fb2115803ca22a2040fbbcb036
Fachri Hawari <fachri.hawari@gmail.com>
2023-09-07 03:39:43 +0700

    add prisma module & test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { Book } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      message: this.appService.getHello()
+    }
+  }
+
+  @Get('/1st')
+  async getFirstBook(): Promise<Book> {
+    return this.prismaService.book.findFirst()
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+const firstBook = {
+  title: "Sang Pencerah",
+  author: "Anonim",
+  price: 59000,
+  publishedYear: 2022,
+  stock: 10,
+  description: "ini buku yg oke"
+}
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +27,39 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  beforeAll(async () => {
+    await prisma.book.create({
+      data: firstBook
+    })
+    await prisma.$disconnect()
+  })
+
+  afterAll(async () => {
+    await prisma.$executeRaw`truncate "books" restart identity cascade;`
+    await prisma.$disconnect()
+  })
+
+  it('/ (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/')
+
+    expect(response.status).toBe(200)
+    expect(response.body).toStrictEqual({
+      message: 'Hello World!'
+    });
+  });
+
+  it('/1st (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/1st')
+
+    expect(response.status).toBe(200)
+    expect(response.body).toBeInstanceOf(Object)
+    expect(response.body.id).toBe(1)
+    expect(response.body.title).toEqual(firstBook.title)
+    expect(response.body.author).toEqual(firstBook.author)
+    expect(response.body.description).toEqual(firstBook.description)
+    expect(response.body.author).toEqual(firstBook.author)
+    expect(response.body.price).toEqual(firstBook.price)
+    expect(response.body.stock).toEqual(firstBook.stock)
+    expect(response.body.publishedYear).toEqual(firstBook.publishedYear)
   });
 });
