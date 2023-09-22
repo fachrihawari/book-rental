289cf82f4aba5e83afa1790c469932bf9d2c45ee
Fachri Hawari <fachri.hawari@gmail.com>
2023-09-22 07:21:42 +0700

    basic books api

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiTags, getSchemaPath } from '@nestjs/swagger';
+import { BooksService } from './books.service';
+import { Book } from 'src/prisma/models/book';
+import { Public } from 'src/auth/auth.decorators';
 
+@ApiTags('Books')
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+    constructor(private readonly booksService: BooksService) {}
+
+    @Get()
+    @Public()
+    @ApiOkResponse({ 
+        description: 'Returns all books.',
+        schema: { items: { $ref: getSchemaPath(Book) } }
+    })
+    findAll() {
+        return this.booksService.findAll();
+    }
+
+    @Get(':id')
+    @Public()
+    @ApiOkResponse({ 
+        description: 'Returns a book by id.',
+        schema: { $ref: getSchemaPath(Book) }
+    })
+    findById(@Param('id') id: string) {
+        return this.booksService.findById(+id);
+    }
+}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from 'src/prisma/models/book';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<Book[]> {
+    return this.prismaService.book.findMany();
+  }
+
+  async findById(id: number): Promise<Book> {
+    return this.prismaService.book.findUnique({
+      where: { id },
+    });
+  }
+}
